72bfa0ca970f705fce032eb2e9c4cfd6f829363f
maxCapote <maximillian.capote@mymail.champlain.edu>
2020-09-25 21:38:10 -0400

    reasonable prototype

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # HomepageEnumeration
+* Simple enumeration tool for web application testing. Hyperlinks are extracted from a given URL and the domain as well as subdomain is determined for each. The SSL expiration date of each site is checked too, if applicable. This project was part of the application process for BreachLock.
+* Example usage
+  * python enumerate.py -u 'https://www.google.com/'
+  * python enumerate.py -u 'https://www.google.com/' -e 'google.csv'
 * Primary references
   * beautifulsoup4
     * https://pypi.org/project/beautifulsoup4/
     * https://www.crummy.com/software/BeautifulSoup/bs4/doc/
   * tldextract
     * https://pypi.org/project/tldextract/
-* Simple enumeration tool for web application testing. Hyperlinks are extracted from a given URL and the domain as well as subdomain is determined for each. The SSL expiration date of each site is checked too, if applicable. This project was part of the application process for BreachLock.
-* Example usage
-  * python enumerate.py -u 'https://www.google.com/'
-  * python enumerate.py -u 'https://www.google.com/' -e 'google.csv'
+  * SSL info
+    * https://stackoverflow.com/questions/45810069/how-to-fetch-the-ssl-certificate-value-whether-its-expired-or-not/52298575
+    * https://stackoverflow.com/questions/30862099/how-can-i-get-certificate-issuer-information-in-python
--- enumerate.py
+++ enumerate.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 from tldextract import extract
+import ssl
+import socket
 import pandas as pd
 import argparse
 
@@ -12,10 +14,7 @@ def extract_hyperlinks(webpage):
     # parse the HTML retrieved
     soup = BeautifulSoup(webpage, 'html.parser')
     # extract all hyperlinks (specifically denoted by <a> tags)
-    links = []
-    for link in soup.find_all('a'):
-        links.append(link.get('href'))
-    return links
+    return [link.get('href') for link in soup.find_all('a')]
 
 def clean_hyperlinks(url, links):
     # remove telephone numbers and unnecessary javascript
@@ -33,13 +32,29 @@ def get_domain_info(links):
     # extract domin and subdomain from each link
     return [[extract(link).domain, extract(link).subdomain] for link in links]
 
-def get_ssl_expiry_date():
-    pass
+def get_ssl_expiry_dates(links):
+    dates = []
+    for link in links:
+        try:
+            # create a new context with secure default settings
+            ctx = ssl.create_default_context()
+            # establish a connection with the given link and obtain the cert
+            with ctx.wrap_socket(socket.socket(), server_hostname=link[8:]) as s:
+                s.connect((link[8:], 443))
+                cert = s.getpeercert()
+            # extract the expiry date
+            dates.append(cert['notAfter'])
+        except:
+            # catch error (e.g. getaddrinfo failure)
+            dates.append('could not be resolved')
+    return dates
 
-def compile_results(links, domain_info):
+def compile_results(links, domain_info, expiry_dates):
+    # derive separate domain and subdomain lists for easier use with pandas
     domains = [di[0] for di in domain_info]
     subdomains = [di[1] for di in domain_info]
-    return pd.DataFrame({'Link': links, 'Domain': domains, 'Subdomin': subdomains})
+    return pd.DataFrame({'Link': links, 'Domain': domains, 'Subdomin': subdomains,
+                         'SSL Expiry Date': expiry_dates})
 
 def Main():
     parser = argparse.ArgumentParser(description = 'command-line args')
@@ -50,7 +65,8 @@ def Main():
         links = extract_hyperlinks(get_webpage(args.url))
         links = clean_hyperlinks(args.url, links)
         domain_info = get_domain_info(links)
-        results = compile_results(links, domain_info)
+        expiry_dates = get_ssl_expiry_dates(links)
+        results = compile_results(links, domain_info, expiry_dates)
         print(results)
         if args.export is not None:
             results.to_csv(args.export, index=False)
